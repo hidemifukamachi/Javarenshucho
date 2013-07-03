89b40c690304982855d87175000b7f6ad10f5ac9
hidemifukamachi <hidemi.fukamachi@gmail.com>
2013-07-03 14:27:00 +0900

    1-4

---
+++ Book.java
@@ -0,0 +1,18 @@
+class Book{
+	String title;
+	int price;
+	String author;
+	Date published;
+
+	void printTitle(){
+		System.out.println(this.title)
+	}
+}
+//書物クラスの利用例
+Book book1 new Book();
+book1.title = "Peopleware";
+book1.printTitle();
+Book book2 = new Book();
+book2.title = "showstopper";
+System.out.println(book2.title);
+
